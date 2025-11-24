7d59e0901f955b5ccf975173d503a84a13381431
on-that-tiger-mountain <15920418725@139.com>
2025-11-25 03:49:19 +0800

    Revert "amstream: Avoid shadowing in ddraw_meminput_Receive()."
    This reverts commit 3a4237c4b9d8924cbce44663487abb783a2fe189.

--- dlls/amstream/ddrawstream.c
+++ dlls/amstream/ddrawstream.c
@@ -1484,7 +1484,7 @@ static HRESULT WINAPI ddraw_meminput_GetAllocatorRequirements(IMemInputPin *ifac
     return E_NOTIMPL;
 }
 
-static HRESULT WINAPI ddraw_meminput_Receive(IMemInputPin *iface, IMediaSample *buffer)
+static HRESULT WINAPI ddraw_meminput_Receive(IMemInputPin *iface, IMediaSample *sample)
 {
     struct ddraw_stream *stream = impl_from_IMemInputPin(iface);
     BITMAPINFOHEADER *bitmap_info;
@@ -1501,13 +1501,13 @@ static HRESULT WINAPI ddraw_meminput_Receive(IMemInputPin *iface, IMediaSample *
     int stride;
     HRESULT hr;
 
-    TRACE("stream %p, buffer %p.\n", stream, buffer);
+    TRACE("stream %p, sample %p.\n", stream, sample);
 
-    hr = IMediaSample_GetPointer(buffer, &pointer);
+    hr = IMediaSample_GetPointer(sample, &pointer);
     if (FAILED(hr))
         return hr;
 
-    IMediaSample_GetTime(buffer, &start_time, &end_time);
+    IMediaSample_GetTime(sample, &start_time, &end_time);
 
     EnterCriticalSection(&stream->cs);
 
